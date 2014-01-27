5c19a9e7518d90828ca49aaa2fe8a037841d6f72
frigo <johndoe@example.com>
2014-01-28 00:15:35 +0100

    with admin doc

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+from django.contrib.admindocs import urls as admindocurls
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -8,6 +9,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'mysite.views.home', name='home'),
     url(r'^$', include('blob.urls')),
     url(r'^blob/', include('blob.urls')),
+    url(r'^admin/doc/', include(admindocurls)),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include('api.urls')),
 )
