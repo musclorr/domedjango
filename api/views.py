6ad32ea5e528b7e727ec33a1df3498cbd868a2f2
frigo <musclorr@gmail.com>
2014-02-03 23:47:56 +0100

    pagination, filtering, search

--- api/views.py
+++ api/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from blob.models import Blob
 from rest_framework.generics import ListCreateAPIView, RetrieveAPIView
+from rest_framework import filters
 from .serializers import BlobSerializer
 
 class BlobList(ListCreateAPIView):
 	model = Blob
 	serializer_class = BlobSerializer
+	filter_fields = ("datext", "someotherfield","other__sometext")
+	filter_backends = (filters.SearchFilter, filters.DjangoFilterBackend)
+	search_fields = ('other__sometext',)
 
 class OneBlob(RetrieveAPIView):
 	model = Blob
--- mysite/settings.py
+++ mysite/settings.py
@@ -95,5 +95,7 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    ],
+	'DEFAULT_FILTER_BACKENDS': ('rest_framework.filters.DjangoFilterBackend',),
+	'PAGINATE_BY': 6, 
 }
